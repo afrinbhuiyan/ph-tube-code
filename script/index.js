8aa156b4e32f89ae68d6ee97530b5226fe97e351
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-03-11 23:55:16 +0600

    -Load Categories & send to display function

---
+++ script/index.js
@@ -0,0 +1,13 @@
+function loadCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  .then((res) => res.json())
+  .then(data => displayCategories(data.categories))
+}
+
+function displayCategories(categories) {
+    console.log(categories)
+}
+
+loadCategories();
+
+// -Load Categories & send to display function
