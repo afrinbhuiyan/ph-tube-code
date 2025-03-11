1da255e9860bdce5160c2418931f47defd2fc1f7
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-03-12 05:54:25 +0600

    load video data & display video card

--- script/index.js
+++ script/index.js
@@ -1,18 +1,55 @@
 function loadCategories() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then((res) => res.json())
-  .then(data => displayCategories(data.categories))
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
+}
+
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
 }
 
 function displayCategories(categories) {
-    const categoryContainer = document.getElementById("category-container");
-    for(let category of categories){
-        const categoryDiv = document.createElement("div")
-        categoryDiv.innerHTML = `
+  const categoryContainer = document.getElementById("category-container");
+  for (let category of categories) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
         <button class="btn text-base bg-zinc-300 hover:bg-[#FF1F3D] hover:text-white">${category.category}</button>
-        `
-        categoryContainer.appendChild(categoryDiv)
-    }
+        `;
+    categoryContainer.appendChild(categoryDiv);
+  }
 }
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    console.log(video);
+
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+      <div class="card bg-base-100">
+  <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title"> ${video.title} </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    `;
+
+    videoContainer.appendChild(videoCard);
+  });
+};
+
 loadCategories();
+
+loadVideos();
